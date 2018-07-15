6f348bf04b467d44e02b92432ad0aa79f77fa7f9
Daniel Beeman <dlb@Daniels-MacBook-Pro-3.local>
2018-07-15 12:59:01 -0700

    included the Cron job and added Michaels node

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,14 @@
 node ip-10-0-1-27{
+    cron { "puppet update":
+        command => "cd /etc/puppet && git pull -q origin master",
+	user    => root,
+        minute => "*/5",
+    }
 }
 
 node ip-10-0-1-189 {
 } 
 
+node ip-10-0-1-214 {
+}
 
