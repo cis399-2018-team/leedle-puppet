31e115d4c7a764a5570294de339dbfaed5336944
Michael Hagel <michaelhagel@Michaels-MacBook-Pro.local>
2018-07-18 18:26:52 -0700

    Initial commit of init.pp for httpd module

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -1,1 +1,15 @@
 #httpd module
+
+class httpd {
+	package { 
+		"apache2": ensure => installed;
+	}
+
+	service { "httpd":
+		enable => true,
+		ensure => running,
+		hasstatus => true,
+		hasrestart => true,
+		require => Package["apache2"], 
+	}
+}
