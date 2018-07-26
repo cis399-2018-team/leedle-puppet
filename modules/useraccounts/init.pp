303763f21a4ffb4e14a7da3b262eeaeb594d3408
Michael Hagel <michaelhagel@Michaels-MacBook-Pro.local>
2018-07-25 19:04:51 -0700

    Initial commit of user account stuff

---
+++ modules/useraccounts/init.pp
@@ -0,0 +1,11 @@
+class useraccounts {
+	$users = ['jbeder, 'dbeeman', 'mhagel2']
+
+	$users.each |String $user| {
+		user { $user:
+			ensure => present,
+			home => 'home/${user}',
+			shell => '/bin/bash',
+		}
+	}
+}
