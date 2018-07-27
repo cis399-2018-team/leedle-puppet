bb9f1eab3123d135f4ae178b2d26cdb6aa6f069a
jakebeder <jakebeder@gmail.com>
2018-07-26 20:10:54 -0700

    Fixed syntax error

--- modules/useraccounts/manifests/init.pp
+++ modules/useraccounts/manifests/init.pp
@@ -1,23 +1,24 @@
 class useraccounts {
-  2         user {'stevev':
-  3                 name => 'stevev',
-  4                 ensure => present,
-  5                 managehome => true,
-  6                 shell => '/bin/bash',
-  7                 require => File['/home/stevev/.ssh'],
-  8         }
-  9
- 10         file { '/home/stevev/.ssh':
- 11                 ensure => 'directory',
- 12                 owner => 'stevev',
- 13         }
- 14         ssh_authorized_key {"stevev":
- 15                 ensure => present,
- 16                 user => "stevev",
- 17                 type => "ssh-rsa",
- 18                 key => "AAAAB3NzaC1yc2EAAAADAQABAAACAQDN0n7TZj/XUCs4ywvPLHfAFTHHkzFbxTW5GuHXMWIyrb7M84ySFSswd2n    45Kh2IQey7sd97V0dj/EENKvy+5FCyyTTLWmnTXzTLMeb5qpV6Xful5vt6KjHd41ZAT2pKoK+yAjcTDRdMFq0QYDUEFug8ETv1/wPsJPgCLAlp0    kJWJq8Q7AqmdCs+NilLvKKZUu62AMWNdNwDDBHCKgtn8TzgsEIxOgFgrrAXz+UdW5z4DLcmGx7wkdAAM0Q18ohC6L0WayiceGHuC/vHfHNklHOW    oBF5L8YWRdGKEN40LMiu//31uuXrmV3ZrudbyhsRcWXr5dukbqD43PpsBzIOo3080FyZJWeLOpbE/6VsrNNlOtroKnuO+mIWOVsBQCH5R3UPwwx    Ttf8yyO7GvNm2HjRBgGLGXql3RhqIpTEVUfzXqadX+b/v8ChuBjYtegr2CWhs85qGj/2gNanZjfE8NJVa8OymuiQi5boRGsoDKXY1F7hnaZNL4C    YyW1yAp44evDyohp/E9+YY3I/RdUfZ/apelHIVNXL4bx25oPUT7/jqqJi3RcyDQugZLH55QWiE1NkFqJqwTif6b6hcsdhIlVuNhzuXKneTm74PC    Q13Ui1nY9mx00wjnG6bXiHLy1mKXshl7i8LX9aK7hAzTeSWPnKJpJ2+UeLrywEp3FKsRbt1Q==",
- 19 	    }
- 20 }
+          user {'stevev':
+                  name => 'stevev',
+                  ensure => present,
+                  managehome => true,
+                  shell => '/bin/bash',
+                  require => File['/home/stevev/.ssh'],
+           }
+ 
+          file { '/home/stevev/.ssh':
+                  ensure => 'directory',
+                  owner => 'stevev',
+          }
+          ssh_authorized_key {"stevev":
+                  ensure => present,
+                  user => "stevev",
+                  type => "ssh-rsa",
+  	    	  key => "AAAAB3NzaC1yc2EAAAADAQABAAACAQDN0n7TZj/XUCs4ywvPLHfAFTHHkzFbxTW5GuHXMWIyrb7M84ySFSswd2n45Kh2IQey7sd97V0dj/EENKvy+5FCyyTTLWmnTXzTLMeb5qpV6Xful5vt6KjHd41ZAT2pKoK+yAjcTDRdMFq0QYDUEFug8ETv1/wPsJPgCLAlp0kJWJq8Q7AqmdCs+NilLvKKZUu62AMWNdNwDDBHCKgtn8TzgsEIxOgFgrrAXz+UdW5z4DLcmGx7wkdAAM0Q18ohC6L0WayiceGHuC/vHfHNklHOWoBF5L8YWRdGKEN40LMiu//31uuXrmV3ZrudbyhsRcWXr5dukbqD43PpsBzIOo3080FyZJWeLOpbE/6VsrNNlOtroKnuO+mIWOVsBQCH5R3UPwwxTtf8yyO7GvNm2HjRBgGLGXql3RhqIpTEVUfzXqadX+b/v8ChuBjYtegr2CWhs85qGj/2gNanZjfE8NJVa8OymuiQi5boRGsoDKXY1F7hnaZNL4CYyW1yAp44evDyohp/E9+YY3I/RdUfZ/apelHIVNXL4bx25oPUT7/jqqJi3RcyDQugZLH55QWiE1NkFqJqwTif6b6hcsdhIlVuNhzuXKneTm74PCQ13Ui1nY9mx00wjnG6bXiHLy1mKXshl7i8LX9aK7hAzTeSWPnKJpJ2+UeLrywEp3FKsRbt1Q==",	
+		
+}
+  }
 
 
 
