1a92e9ab21fa4c7db1463107c53af1d0d58cea42
jakebeder <jakebeder@gmail.com>
2018-07-27 15:18:26 -0700

    added quinn as a user

--- modules/useraccounts/manifests/init.pp
+++ modules/useraccounts/manifests/init.pp
@@ -17,23 +17,38 @@ class useraccounts {
 
   }
 
-          user {'miguel':
-                  name => 'miguel',
+          user {'mhernan7':
+                  name => 'mhernan7',
 		  uid => '1002',
                   ensure => present,
-		  home => '/home/miguel/',
+		  home => '/home/mhernan7/',
                   managehome => true,
                   shell => '/bin/bash',
 	
 	} 
-          ssh_authorized_key {"miguel":
+          ssh_authorized_key {"mhernan7":
                   ensure => present,
-                  user => "stevev",
+                  user => "mhernan7",
                   type => "ssh-rsa",
   	    	  key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCBz/FWaJSh15HkEcACB6Wh3jwT7EnCGr6pYFDuuqtXkHl2z4nIMHtT79azu/6jEwAC5664YBe8LLlVYUfgox4bCnlMbCbd+CXkUVviRNr3J/CH2vyK80zX9kLI0C3A6IJjzfweNOC/BRjvUHK0+GZlfdspEEinyEbD0UvAm1KFYaxYSNT23cFSwM422ojDlsEI/KWgcKTmtvmsEbJUcTHjx4nWCP69OEvZuH392wbZISyhEXnbINxnfmEho13fmk/qOO0MRA/LT9zT7OPUJjsd3X0qrabLu+W6PZbqysRe/ZGIiMC+YJXoUn/7QNbg9V4faBI0PTIMefW1qMtiHqM7",
 
 }
+	   user {'quinnmil':
+                  name => 'quinnmil',
+		  uid => '1003',
+                  ensure => present,
+		  home => '/home/quinnmil/',
+                  managehome => true,
+                  shell => '/bin/bash',
+	
+	} 
+          ssh_authorized_key {"quinnmil":
+                  ensure => present,
+                  user => "quinnmil",
+                  type => "ssh-rsa",
+  	    	  key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCL9F71YgYpfFYpFfeOqLlRlBai6X9niYCMmBOmSqIVnL81NF9b0XfNkSMgv4/6/ab3TXm9GVTKYY3cSr5m/K0ufNS4DZ3e7U9gyEiYutbXBohYXZ/ngh98WGOf2wS2Y2s5+XLrI+vauLA/QHjtLesh6QPZXxWRt63io9+ZdUHeWkMphGLvDyzd8QwM9mRJmNGxb2dP34ACTgGYgCGKzsLgEJ7ADbUC30GRhvPNqX3w+DXXGF/RmP7kknCrkesjhH7m+IQeFF1Uppaexai5EOiNQks5G6Bmkfbb8l+SRpCvcXxDSrK+42uhQkt7tbgy7j1YbjpBbtaE0pViHIV+Mi6V",
 
+}
 
 
 
