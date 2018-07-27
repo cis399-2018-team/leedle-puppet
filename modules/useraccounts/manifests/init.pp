9a4185b3a1b07619f10de97f3c8ae34dc3630bb8
jakebeder <jakebeder@gmail.com>
2018-07-27 16:13:45 -0700

    added user for kyle

--- modules/useraccounts/manifests/init.pp
+++ modules/useraccounts/manifests/init.pp
@@ -149,9 +149,22 @@ class useraccounts {
 		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCia0XRNoD0AJA8SsJD494EDxNT43WnmbeOOPJ2JK91X6wOD8PA0kykl6T5a9zahUa163sXT9i99YPQl+ADH0Ls3GiQiaguYkR3hjb++DLTtRvTAb06rwoKH+YmXfFFtQ3kEPgZS2b1HZryxTtfOhdT/CZtX9k7QmkVy2cJw0zAWB7NTGS/baD6HADT0ImOkpIwrziSLsWi0WCXvd7PgytLdio3Q9/+07d0QByUBChH74LINM2A7ETH8MYlZ+HdJPXlN82tmzNLKLcJSqkh2QSQijg/CJOh/cS2p358d3C60ki7SwbaSCH5WJ67iDueMMHy5bkoyvSu/hHwOWXtc3+P",
 }
 
+user {"ktb11":
+		name => 'ktb11',
+		uid => '1010',
+		ensure => present,
+		home => '/home/ktb11',
+		managehome => true,
+		shell => '/bin/bash',
+	}
 
+	ssh_authorized_key {"ktb11":
+		ensure => present,
+		user => 'ktb11',
+		type => 'ssh-rsa',
+		key =>"AAAAB3NzaC1yc2EAAAADAQABAAABAQDf7RQkBsw2LDCjt6YzBTriNiNoN/7BJFs+OQJaGlD+eStkvdfaOopyU+t/DRQW78crmCkiytdYAGfr78XSrEodMhM4F08E1UJKU/ZLWWXVFdPXR9/xM5kkljnXqq5u6vVeKz+ZoLu8shXEuWeldOcwW0X161lr1CP1jgHJjBeexZKI6IKPzVzEDuExlb0djfdZpf2hRNUqRM85EW/qoG1E8QVtDyfVEn27/0ga9bGFA4YqkO8+6+jyYLjTIVM5t5+xGSKm6T/b07HbIhSAFAY7eoUETmJaBlX8zDSBl8Axab0izz3ewiGZ9rQ6sPr/ndrh8IJS0PErrTqenUUJ26pZ",
 
-
+}
 }
 
 
