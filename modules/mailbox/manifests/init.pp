bc261e17d2e8c46ca90abd4d4ea9bd0bec4c14d8
Michael Hagel <michaelhagel@Michaels-MacBook-Pro.local>
2018-08-10 19:20:23 -0700

    Few changes to mailbox puppet module

--- modules/mailbox/manifests/init.pp
+++ modules/mailbox/manifests/init.pp
@@ -16,7 +16,7 @@ class mailbox {
 		require => Package["postfix"],
 	}
 
-	file {"mutt one not sure yet:
+	file {"~/.muttrc":
 		mode =>444,
 		owner =>root,
 		group => root,
@@ -29,9 +29,9 @@ class mailbox {
 		ensure => running,
 		hasstatus =>true,
 		hasrestart => true,
-		require => [Package["postfix"], Package["mutt"],
-				File[/etc/postfix/main.cf],FILE[UNKNOWN]],
-		subscribe => [File["/etc/postfix/main.cf"], FILE[UNKNOWN]],
+		require => [Package["postfix"],	
+				File[/etc/postfix/main.cf]],
+		subscribe => [File["/etc/postfix/main.cf"],
 		}
 
 }
